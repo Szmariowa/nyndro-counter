5b5dfe4d57b6012b0df1356c360f77b3a887cc2f
Szmariowa <justyna.szalanska1@gmail.com>
2021-10-20 21:12:44 +0200

    print 1 item

--- counter_users.py
+++ counter_users.py
@@ -4,7 +4,15 @@ import os.path
 
 if os.path.isfile("countery_users.txt"):
     open_file = open("countery_users.txt", "r")
+    
+
     for x in open_file:
-        print(x)
+        data = x.strip().split(',')
+        print(data[0])
+     
 
     open_file.close()
+
+
+
+
